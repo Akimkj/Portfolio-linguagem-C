f220654b0aaeb80367e0702208d18a3b490f2229
Mika <kakamarques.0202@gmail.com>
2025-03-13 12:15:19 -0300

    add aulas e ex

---
+++ exercicios/ex001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int idade;
+    float peso, altura;
+    char sexo;
+    printf("Informe, respectivamente, a sua idade, sexo(m, f ou n), peso e altura: ");
+    scanf("%d", &idade);
+    printf("%d", idade);
+}
