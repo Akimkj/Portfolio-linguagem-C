49b17c078a3a0cedeca64e9013f575bfdd6d5451
Mika <kakamarques.0202@gmail.com>
2025-03-16 20:49:35 -0300

    add aulas

--- exercicios/ex001.c
+++ exercicios/ex001.c
@@ -5,21 +5,21 @@ int main() {
     int idade;
     float peso, altura;
     char sexo;
-    printf("Informe, respectivamente, a sua idade, sexo(m, f ou n), peso e altura: ");
-    scanf("%d %c %f %f", &idade, &sexo, &peso, &altura);
+    /*printf("Informe, respectivamente, a sua idade, sexo(m, f ou n), peso e altura: ");
+    scanf("%d %c %f %f", &idade, &sexo, &peso, &altura);*/
 
-    /*printf("Informe sua idade: ");
+    printf("Informe sua idade: ");
     scanf("%d", &idade);
-    getchar();
+    
     printf("\nInforme seu sexo (m, f ou n): ");
-    scanf("%c", &sexo);
-    getchar();
+    scanf(" %c", &sexo);
+    
     printf("\nInforme seu peso: ");
-    scanf("%f", &peso);
-    getchar();
+    scanf(" %f", &peso);
+    
     printf("\nInforme sua altura: ");
-    scanf("%f", &altura);
-    getchar();*/
+    scanf(" %f", &altura);
+    
 
     printf(" Sua idade: %d\n Seu sexo: %c\n Seu peso: %.2f\n Sua altura: %.2f\n", idade, sexo, peso, altura);
     
